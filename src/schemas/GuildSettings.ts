6b7539b36d4220411edd6f18aad962525f844a6f
Miqhtie <miqhtiedev@gmail.com>
2021-01-09 02:03:31 -0500

    feat: c!verbal

--- src/schemas/GuildSettings.ts
+++ src/schemas/GuildSettings.ts
@@ -2,13 +2,17 @@ import { Schema, model, Document } from "mongoose";
 export interface IGuildSettings extends Document {
   guildID: string;
   disabledCommands: Array<string>;
+  verbals: Array<any>;
+  punishmentcases: number;
   sleep: Boolean;
 }
 
 const GuildSettingsScema = new Schema({
   guildID: { type: String, requried: true, index: true, unique: true },
   disabledCommands: { type: Array<string>(), default: new Array<String>() },
-  sleep: {type: Boolean, default: false}
+  verbals: { type: Array<any>(), default: new Array<any>() },
+  punishmentcases: {type: Number, default: 1},
+  sleep: { type: Boolean, default: false },
 });
 
 export default model<IGuildSettings>("GuildSettings", GuildSettingsScema);
