270eb8bcaf26aa1c085163c8c4d09f612d7a9689
Joel <joelcedras@gmail.com>
2021-02-07 17:16:54 +0200

    deletes command issuer's message

--- src/commands/verbal.ts
+++ src/commands/verbal.ts
@@ -61,6 +61,7 @@ module.exports = {
 
       const embed = new MessageEmbed().setTitle(`${member.user.tag} has been verbal warned! Case: ${casenumber}`).setColor("#48db8f");
       message.channel.send(embed);
+      message.delete();
     } else if (args[0] === "remove") {
       // c!verbal remove (casenumber)
       if (args.length < 2) {
