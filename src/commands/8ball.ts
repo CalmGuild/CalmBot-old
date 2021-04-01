5092d5625b8dab2bd53dd12a2f2b1d25270a4db3
Bushman <aidanthegreat765@gmail.com>
2021-04-01 12:27:04 -0400

    Update 8ball.ts

--- src/commands/8ball.ts
+++ src/commands/8ball.ts
@@ -22,7 +22,7 @@ const responses = [
   "My sources say no",
   "Outlook not so good",
   "Very doubtful",
-  "I DONT FEEL LIKE ANSWERING THAT, CONTINUE ON WITH YOUR DAY"
+  "I DONT FEEL LIKE ANSWERING THAT, CONTINUE ON WITH YOUR DAY",
 ];
 
 
