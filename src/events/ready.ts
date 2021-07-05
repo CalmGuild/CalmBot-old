021a738b89c63b1469da816c56c3bf137c96f052
Miqhtiedev <miqhtiedev@gmail.com>
2021-07-05 00:31:26 -0400

    have bot set status in ready event

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -3,4 +3,5 @@ import Logger from "../utils/logger/Logger";
 
 export default function ready(client: Client) {
   Logger.info(`${client.user?.tag} is now serving in ${client.guilds.cache.size} guilds!`);
-};
+  client.user?.setActivity("a server full of idiots", { type: "WATCHING" });
+}
